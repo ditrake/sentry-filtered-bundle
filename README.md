c16abb8583cd5c7819ad68dc75006e392755b1e6
Sergey Razin <srr@webcrt.ru>
2020-02-07 16:50:05 +0500

    end first version

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
 # Sentry filtered bundle
+
+For simple configured:
+
+```yaml
+# config/services.yaml
+
+parameters:
+  sentry_filtered:
+    filtered_exceptions: 
+      - 'Your/Awesome/Exception'
+      - 'Your/Another/Awesome/Exception/'
+```
+
+And in `config/packages/sentry.yaml`
+
+```yaml
+# config/packages/sentry.yaml
+
+sentry:
+  options:
+    before_send: '@sentry_filtered'
+```
--- src/Service/SentryErrorFilter.php
+++ src/Service/SentryErrorFilter.php
@@ -7,6 +7,7 @@ declare(strict_types=1);
 
 namespace srr\SentryFilteredBundle\Service;
 
+use Psr\Log\LoggerInterface;
 use srr\SentryFilteredBundle\Exception\SentryFilterException;
 use Sentry\Event;
 use Symfony\Component\DependencyInjection\ContainerInterface;
@@ -23,14 +24,20 @@ class SentryErrorFilter
      * @var ContainerInterface
      */
     private ContainerInterface $container;
+    /**
+     * @var LoggerInterface
+     */
+    private LoggerInterface $logger;
 
     /**
      * SentryErrorFilter constructor.
      * @param ContainerInterface $container
+     * @param LoggerInterface    $logger
      */
-    public function __construct(ContainerInterface $container)
+    public function __construct(ContainerInterface $container, LoggerInterface $logger)
     {
         $this->container = $container;
+        $this->logger = $logger;
     }
 
     public function __invoke(Event $event): ?Event
@@ -51,8 +58,13 @@ class SentryErrorFilter
         }
 
         $parameters = $this->container->getParameter(self::CONFIGURATION_ROOT);
-        if (!isset($parameters[self::FILTERED_EXCEPTIONS]) || empty($parameters[self::FILTERED_EXCEPTIONS]) || !is_array($parameters[self::FILTERED_EXCEPTIONS])) {
+        if (!isset($parameters[self::FILTERED_EXCEPTIONS]) || !is_array($parameters[self::FILTERED_EXCEPTIONS])) {
             throw new SentryFilterException(\sprintf('Parameters \'%s\' for sentry filter not configured', self::FILTERED_EXCEPTIONS));
+        }
+
+        if (empty($parameters[self::FILTERED_EXCEPTIONS])) {
+            $this->logger->warning(\sprintf('Parameters `%s` for sentry filter is empty.', self::FILTERED_EXCEPTIONS));
+            return true;
         }
 
         foreach ($parameters[self::FILTERED_EXCEPTIONS] as $exception) {
